98b5752a9ed990ad51888a12fa53be5958beaae1
davidyamd <86858903+davidyamd@users.noreply.github.com>
2021-07-04 01:28:01 -0400

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,284 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnSalir = New System.Windows.Forms.Button()
+        Me.BtnLimpiar = New System.Windows.Forms.Button()
+        Me.BtnComenzar = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TxtHoraMenorTemperatura = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.TxtMenorTemperatura = New System.Windows.Forms.TextBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TxtMayorTemperatura = New System.Windows.Forms.TextBox()
+        Me.TxtHoraMayorTemperatura = New System.Windows.Forms.TextBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TxtTemperaturaPromedio = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'BtnSalir
+        '
+        Me.BtnSalir.BackColor = System.Drawing.Color.Silver
+        Me.BtnSalir.Location = New System.Drawing.Point(255, 281)
+        Me.BtnSalir.Name = "BtnSalir"
+        Me.BtnSalir.Size = New System.Drawing.Size(75, 45)
+        Me.BtnSalir.TabIndex = 0
+        Me.BtnSalir.Text = "Salir"
+        Me.BtnSalir.UseVisualStyleBackColor = False
+        '
+        'BtnLimpiar
+        '
+        Me.BtnLimpiar.BackColor = System.Drawing.Color.Silver
+        Me.BtnLimpiar.Enabled = False
+        Me.BtnLimpiar.Location = New System.Drawing.Point(167, 281)
+        Me.BtnLimpiar.Name = "BtnLimpiar"
+        Me.BtnLimpiar.Size = New System.Drawing.Size(75, 45)
+        Me.BtnLimpiar.TabIndex = 1
+        Me.BtnLimpiar.Text = "Limpiar"
+        Me.BtnLimpiar.UseVisualStyleBackColor = False
+        '
+        'BtnComenzar
+        '
+        Me.BtnComenzar.BackColor = System.Drawing.Color.Silver
+        Me.BtnComenzar.Location = New System.Drawing.Point(196, 48)
+        Me.BtnComenzar.Name = "BtnComenzar"
+        Me.BtnComenzar.Size = New System.Drawing.Size(80, 45)
+        Me.BtnComenzar.TabIndex = 2
+        Me.BtnComenzar.Text = "Comenzar"
+        Me.BtnComenzar.UseVisualStyleBackColor = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.BtnComenzar)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(472, 104)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(13, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(447, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Presione ""Comenzar"" para calcular las temperaturas del día:"
+        '
+        'TxtHoraMenorTemperatura
+        '
+        Me.TxtHoraMenorTemperatura.BackColor = System.Drawing.SystemColors.Info
+        Me.TxtHoraMenorTemperatura.Enabled = False
+        Me.TxtHoraMenorTemperatura.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtHoraMenorTemperatura.Location = New System.Drawing.Point(10, 35)
+        Me.TxtHoraMenorTemperatura.Name = "TxtHoraMenorTemperatura"
+        Me.TxtHoraMenorTemperatura.Size = New System.Drawing.Size(100, 28)
+        Me.TxtHoraMenorTemperatura.TabIndex = 4
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.Label17)
+        Me.GroupBox2.Controls.Add(Me.TxtMenorTemperatura)
+        Me.GroupBox2.Controls.Add(Me.TxtHoraMenorTemperatura)
+        Me.GroupBox2.Enabled = False
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 122)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(233, 80)
+        Me.GroupBox2.TabIndex = 5
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Menor temperatura"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(202, 35)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 24)
+        Me.Label2.TabIndex = 43
+        Me.Label2.Text = "°C"
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label17.Location = New System.Drawing.Point(112, 35)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(15, 24)
+        Me.Label17.TabIndex = 42
+        Me.Label17.Text = ":"
+        '
+        'TxtMenorTemperatura
+        '
+        Me.TxtMenorTemperatura.BackColor = System.Drawing.SystemColors.Info
+        Me.TxtMenorTemperatura.Enabled = False
+        Me.TxtMenorTemperatura.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtMenorTemperatura.Location = New System.Drawing.Point(130, 35)
+        Me.TxtMenorTemperatura.Name = "TxtMenorTemperatura"
+        Me.TxtMenorTemperatura.Size = New System.Drawing.Size(70, 28)
+        Me.TxtMenorTemperatura.TabIndex = 5
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label3)
+        Me.GroupBox3.Controls.Add(Me.Label4)
+        Me.GroupBox3.Controls.Add(Me.TxtMayorTemperatura)
+        Me.GroupBox3.Controls.Add(Me.TxtHoraMayorTemperatura)
+        Me.GroupBox3.Enabled = False
+        Me.GroupBox3.Location = New System.Drawing.Point(251, 122)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(233, 80)
+        Me.GroupBox3.TabIndex = 44
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Mayor temperatura"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(202, 35)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(29, 24)
+        Me.Label3.TabIndex = 43
+        Me.Label3.Text = "°C"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(112, 35)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(15, 24)
+        Me.Label4.TabIndex = 42
+        Me.Label4.Text = ":"
+        '
+        'TxtMayorTemperatura
+        '
+        Me.TxtMayorTemperatura.BackColor = System.Drawing.SystemColors.Info
+        Me.TxtMayorTemperatura.Enabled = False
+        Me.TxtMayorTemperatura.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtMayorTemperatura.Location = New System.Drawing.Point(130, 35)
+        Me.TxtMayorTemperatura.Name = "TxtMayorTemperatura"
+        Me.TxtMayorTemperatura.Size = New System.Drawing.Size(70, 28)
+        Me.TxtMayorTemperatura.TabIndex = 5
+        '
+        'TxtHoraMayorTemperatura
+        '
+        Me.TxtHoraMayorTemperatura.BackColor = System.Drawing.SystemColors.Info
+        Me.TxtHoraMayorTemperatura.Enabled = False
+        Me.TxtHoraMayorTemperatura.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtHoraMayorTemperatura.Location = New System.Drawing.Point(10, 35)
+        Me.TxtHoraMayorTemperatura.Name = "TxtHoraMayorTemperatura"
+        Me.TxtHoraMayorTemperatura.Size = New System.Drawing.Size(100, 28)
+        Me.TxtHoraMayorTemperatura.TabIndex = 4
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.Label5)
+        Me.GroupBox4.Controls.Add(Me.TxtTemperaturaPromedio)
+        Me.GroupBox4.Enabled = False
+        Me.GroupBox4.Location = New System.Drawing.Point(12, 208)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(472, 65)
+        Me.GroupBox4.TabIndex = 4
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Temperatura promedio"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(287, 23)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(29, 24)
+        Me.Label5.TabIndex = 44
+        Me.Label5.Text = "°C"
+        '
+        'TxtTemperaturaPromedio
+        '
+        Me.TxtTemperaturaPromedio.BackColor = System.Drawing.SystemColors.Info
+        Me.TxtTemperaturaPromedio.Enabled = False
+        Me.TxtTemperaturaPromedio.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtTemperaturaPromedio.Location = New System.Drawing.Point(186, 21)
+        Me.TxtTemperaturaPromedio.Name = "TxtTemperaturaPromedio"
+        Me.TxtTemperaturaPromedio.Size = New System.Drawing.Size(100, 28)
+        Me.TxtTemperaturaPromedio.TabIndex = 44
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(496, 338)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.BtnSalir)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.BtnLimpiar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents BtnSalir As System.Windows.Forms.Button
+    Friend WithEvents BtnLimpiar As System.Windows.Forms.Button
+    Friend WithEvents BtnComenzar As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TxtHoraMenorTemperatura As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents TxtMenorTemperatura As System.Windows.Forms.TextBox
+    Friend WithEvents Label17 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TxtMayorTemperatura As System.Windows.Forms.TextBox
+    Friend WithEvents TxtHoraMayorTemperatura As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox4 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents TxtTemperaturaPromedio As System.Windows.Forms.TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,122 @@
+﻿'Programa elaborado por DAVID YAUCA / 04-07-2021
+
+'PROBLEMA: Desarrolle un programa que lea las temperaturas registradas 
+'durante las 24 Horas del día. Se pide temperatura promedio del día, mayor 
+'y menor temperatura registrada y en qué hora del día se cumplieron esas 2.
+
+Public Class Form1
+
+    Private Sub BtnComenzar_Click(sender As Object, e As EventArgs) Handles BtnComenzar.Click
+        'ARRAY que almacena las temperaturas de las 24 horas del día
+        Dim TemperaturasDia(23) As Decimal
+        'Variable que valida la entrada del usuario para evitar que sea una cadena vacía
+        Dim Temperatura As String
+
+        'Se recorre el ARRAY para almacenar las temperaturas que ingresa el usuario
+        For i As Integer = 0 To TemperaturasDia.Length - 1
+            Temperatura = InputBox("Ingrese la temperatura de las " & i & ":00 Horas",
+                                   "Ingresar Temperatura",
+                                   "0.0")
+            'Si el usuario pisa "Cancelar" dentro del InputBox o no ingresa ningún dato, 
+            'se detiene el proceso
+            If (Temperatura = "") Then
+                Return
+            Else
+                TemperaturasDia(i) = Val(Temperatura)
+            End If
+        Next
+
+        MsgBox("¡Las temperaturas han sido ingresadas correctamente!",
+               MsgBoxStyle.OkOnly, "Registro exitoso")
+
+        'Se inhabilita el botón "BtnComenzar"
+        BtnComenzar.Enabled = False
+
+        'Variable que almacena la HORA de la MAYOR TEMPERATURA
+        Dim HoraMayorTemperatura As Integer
+
+        'Variable que almacena la MAYOR TEMPERATURA
+        Dim MayorTemperatura As Decimal
+
+        'Determina cuál es la MAYOR temperatura
+        For i As Integer = 0 To TemperaturasDia.Length - 1
+            If (TemperaturasDia(i) > MayorTemperatura) Then
+                HoraMayorTemperatura = i
+                MayorTemperatura = TemperaturasDia(i)
+            End If
+        Next
+
+        'Variable que almacena la HORA de la MENOR TEMPERATURA
+        Dim HoraMenorTemperatura As Integer = 0
+
+        'Variable que almacena la MENOR TEMPERATURA
+        Dim MenorTemperatura As Decimal = MayorTemperatura
+
+        'Determina cuál es la MENOR temperatura
+        For i As Integer = 0 To TemperaturasDia.Length - 1
+            If (MenorTemperatura > TemperaturasDia(i)) Then
+                HoraMenorTemperatura = i
+                MenorTemperatura = TemperaturasDia(i)
+            End If
+        Next
+
+
+        'Variable que almacena la sumatoria de todas las temperaturas
+        Dim AcumuladorTemperatura As Decimal = 0.0
+        'Variable que almacena el promedio de las temperaturas registradas
+        Dim TemperaturaPromedio As Decimal = 0.0
+
+        'Suma todas las temperaturas y almacena el resultado en "AcumuladorTemperatura"
+        For i As Integer = 0 To TemperaturasDia.Length - 1
+            AcumuladorTemperatura += TemperaturasDia(i)
+        Next
+
+        'Se almacena el promedio en "TemperaturaPromedio"
+        TemperaturaPromedio = AcumuladorTemperatura / TemperaturasDia.Length
+
+
+        'Se muestra por pantalla la HORA de la MENOR TEMPERATURA
+        TxtHoraMenorTemperatura.Text = HoraMenorTemperatura & ":00 h"
+        'Se muestra por pantalla la MENOR TEMPERATURA
+        TxtMenorTemperatura.Text = MenorTemperatura
+
+        'Se muestra por pantalla la HORA de la MAYOR TEMPERATURA
+        TxtHoraMayorTemperatura.Text = HoraMayorTemperatura & ":00 h"
+        'Se muestra por pantalla la MAYOR TEMPERATURA
+        TxtMayorTemperatura.Text = MayorTemperatura
+
+        'Se muestra por pantalla el PROMEDIO DE LAS TEMPERATURAS
+        TxtTemperaturaPromedio.Text = TemperaturaPromedio
+
+
+        'Se habilita el botón "BtnLimpiar"
+        BtnLimpiar.Enabled = True
+    End Sub
+
+    Private Sub BtnLimpiar_Click(sender As Object, e As EventArgs) Handles BtnLimpiar.Click
+        TxtHoraMenorTemperatura.Clear()
+        TxtMenorTemperatura.Clear()
+
+        TxtHoraMayorTemperatura.Clear()
+        TxtMayorTemperatura.Clear()
+
+        TxtTemperaturaPromedio.Clear()
+
+        BtnComenzar.Enabled = True
+        BtnLimpiar.Enabled = False
+    End Sub
+
+    Private Sub BtnSalir_Click(sender As Object, e As EventArgs) Handles BtnSalir.Click
+        'Despliega un MessageBox que pide confirmación para salir del programa
+        Dim TextoMsgBox As String = "¿Desea salir de la aplicación?"
+        Dim TituloMsgBox As String = "Salir"
+        Dim BotonesMsgBox As MessageBoxButtons = MessageBoxButtons.YesNo
+
+        Dim MsgBox As DialogResult = MessageBox.Show(TextoMsgBox, TituloMsgBox, BotonesMsgBox)
+
+        'Si la opción del usuario es "Si", se detiene la ejecución del programa
+        If MsgBox = System.Windows.Forms.DialogResult.Yes Then
+            End
+        End If
+    End Sub
+End Class
